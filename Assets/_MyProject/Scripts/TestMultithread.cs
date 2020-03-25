2c82a16944e0d7ce3b1aa84b3771f4cb55aef26a
LongChau1210 <longchau1210@gmail.com>
2020-03-26 00:28:32 +0700

    + Test multithread

---
+++ Assets/_MyProject/Scripts/TestMultithread.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class TestMultithread : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        // Observable.Start is start factory methods on specified scheduler
+        // default is on ThreadPool
+        var heavyMethod = Observable.Start(() =>
+        {
+            // heavy method...
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+            return 10;
+        });
+
+        var heavyMethod2 = Observable.Start(() =>
+        {
+            // heavy method...
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(3));
+            return 10;
+        });
+
+        // Join and await two other thread values
+        Observable.WhenAll(heavyMethod, heavyMethod2)
+            .ObserveOnMainThread() // return to main thread
+            .Subscribe(xs =>
+            {
+                // Unity can't touch GameObject from other thread
+                // but use ObserveOnMainThread, you can touch GameObject naturally.
+                //(GameObject.Find("myGuiText")).GetComponent<GUIText>.text = xs[0] + ":" + xs[1];
+                Debug.Log($"{xs[0]} : {xs[1]}");
+            });
+    }
+}
--- Assets/_MyProject/Scripts/TestOnNextPattern.cs
+++ Assets/_MyProject/Scripts/TestOnNextPattern.cs
@@ -10,14 +10,18 @@ public class TestOnNextPattern : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //// use case
-        //Application.LoadLevelAsync("testscene")
-        //    .AsObservable()
-        //    .Do(x => Debug.Log(x)) // output progress
-        //    .Last() // last sequence is load completed
-        //    .Subscribe();
+        Test_LoadLevel();
     }
 
+    private static void Test_LoadLevel()
+    {
+        // use case
+        Application.LoadLevelAsync("testscene")
+            .AsObservable()
+            .Do(x => Debug.Log(x)) // output progress
+            .Last() // last sequence is load completed
+            .Subscribe();
+    }
 
     private IEnumerator RunAsyncOperation(UnityEngine.AsyncOperation asyncOperation, IObserver<float> observer, CancellationToken cancellationToken)
     {
