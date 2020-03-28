38b24181cd95d7b0225ca1fab23cca7383564862
LongChau1210 <longchau1210@gmail.com>
2020-03-28 17:12:06 +0700

    ~ update

---
+++ Assets/_MyProject/Scripts/DragAndDropOnce.cs
@@ -0,0 +1,16 @@
+﻿using UniRx;
+using UniRx.Triggers; // need UniRx.Triggers namespace for extend gameObejct
+using UnityEngine;
+
+public class DragAndDropOnce : MonoBehaviour
+{
+    void Start()
+    {
+        // All events can subscribe by ***AsObservable
+        this.OnMouseDownAsObservable()
+            .SelectMany(_ => this.UpdateAsObservable())
+            .TakeUntil(this.OnDestroyAsObservable())
+            .Select(_ => Input.mousePosition)
+            .Subscribe(x => Debug.Log(x));
+    }
+}
---
+++ Assets/_MyProject/Scripts/TestTriggers.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx.Triggers;
+using UniRx;
+
+public class TestTriggers : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        // Get the plain object
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        // Add ObservableXxxTrigger for handle MonoBehaviour's event as Observable
+        cube.AddComponent<ObservableUpdateTrigger>()
+            .UpdateAsObservable()
+            .SampleFrame(60)    // run each 30 frame
+            .Subscribe(x => Debug.Log("cube"), () => Debug.Log("destroy"));
+
+        cube.AddComponent<DragAndDropOnce>();
+
+        // destroy after 10 second:)
+        Destroy(cube, 10f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
